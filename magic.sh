20ee0df812298855a6f653b57d371d030ce07ace
Owen <124691791+PiVortex@users.noreply.github.com>
2023-06-28 22:21:30 +0100

    Create magic.sh

---
+++ magic.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Go into the "contract" directory
+cd contract || exit 1
+
+# Set execute permissions for deploy.sh and build.sh
+chmod +x deploy.sh
+chmod +x build.sh
+
+# Run deploy.sh
+./deploy.sh
+
+# Exit the "contract" directory
+cd .. || exit 1
+
+# Go into the "frontend" directory
+cd frontend || exit 1
+
+# Run npm start
+npm start
+
+# Set execute permissions for start.sh
+chmod +x start.sh
+
+# Run npm start again
+npm start
