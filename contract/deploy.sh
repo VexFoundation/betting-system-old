a196851ad461eb9f5b9fcc8972d26171a0c5e4ec
Owen <124691791+PiVortex@users.noreply.github.com>
2023-06-28 21:22:11 +0100

    Add files via upload

---
+++ contract/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+./build.sh
+
+if [ $? -ne 0 ]; then
+  echo ">> Error building contract"
+  exit 1
+fi
+
+echo ">> Deploying contract"
+
+# https://docs.near.org/tools/near-cli#near-dev-deploy
+near dev-deploy ./target/wasm32-unknown-unknown/release/betting_with_frontend.wasm
